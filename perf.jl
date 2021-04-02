2b54d8774dfaf9bbcfdb50a11b27c7b0541f7706
Charles Kawczynski <kawczynski.charles@gmail.com>
2021-04-02 16:22:11 -0700

    Add precompile for hand rank methods

---
+++ perf.jl
@@ -0,0 +1,19 @@
+using PlayingCards
+using Test
+using Combinatorics
+using HoldemHandEvaluator
+
+function benchmark()
+    k = 0
+    for cards in combinations(full_deck(), 5)
+        hr = hand_rank(cards)
+        k+=1
+        k>=100 && break
+    end
+end
+
+function main()
+  @time benchmark()
+end
+
+main()
