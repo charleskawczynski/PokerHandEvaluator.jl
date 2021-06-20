b555b011db371429741a0a6fdaefc3fa1591d5a5
Charles Kawczynski <kawczynski.charles@gmail.com>
2021-06-20 12:12:29 -0700

    Dont cache hand types in che, hand types -> syms
    Make evaluate5 return an Int

--- src/PokerHandEvaluator.jl
+++ src/PokerHandEvaluator.jl
@@ -7,9 +7,6 @@ using Combinatorics
 export CompactHandEval, FullHandEval
 export hand_rank, hand_type, best_cards, all_cards
 
-include("HandTypes.jl")
-using .HandTypes
-
 abstract type AbstractHandEvaluation end
 
 """
@@ -27,8 +24,17 @@ function hand_rank end
 """
     hand_type(::AbstractHandEvaluation)
 
-The hand type, a sub-type of
-[`AbstractHandType`](@ref).
+A Symbol corresponding to the type of hand.
+Concretely, `hand_type` returns one of:
+ - `:straight_flush`
+ - `:quads`
+ - `:full_house`
+ - `:flush`
+ - `:straight`
+ - `:trips`
+ - `:two_pair`
+ - `:one_pair`
+ - `:high_card`
 """
 function hand_type end
 
@@ -65,14 +71,14 @@ player_cards = (
 );
 fhe = FullHandEval.(player_cards);
 winner_id = argmin(hand_rank.(fhe)) # = 2
-winning_hand = hand_type(fhe[winner_id]) # = Trips()
+winning_hand = hand_type(fhe[winner_id]) # = :trips
 winning_rank = hand_rank(fhe[winner_id]) # = 1842
 winning_cards = best_cards(fhe[winner_id]) # = (J♠, T♣, J♡, J♣, 5♣)
 allcards = all_cards(fhe[winner_id]) # = (J♠, T♣, J♡, J♣, 2♣, 3♢, 5♣)
 ```
 """
-struct FullHandEval{HT <: AbstractHandType, AC <: Tuple, TC <: Tuple} <: AbstractHandEvaluation
-    hand_type::HT
+struct FullHandEval{AC <: Tuple, TC <: Tuple} <: AbstractHandEvaluation
+    hand_type::Symbol
     rank::Int
     all_cards::AC
     best_cards::TC
@@ -84,13 +90,11 @@ struct FullHandEval{HT <: AbstractHandType, AC <: Tuple, TC <: Tuple} <: Abstrac
         rank, hand_type, best_cards = evaluate_full(all_cards)
 
         return new{
-            typeof(hand_type),
             typeof(all_cards),
             typeof(best_cards)}(hand_type, rank, all_cards, best_cards)
     end
 end
 
-to_tuple(th::FullHandEval) = (th.rank, th.hand_type, th.best_cards, th.all_cards)
 hand_rank(th::FullHandEval) = th.rank
 hand_type(th::FullHandEval) = th.hand_type
 best_cards(th::FullHandEval) = th.best_cards
@@ -109,7 +113,6 @@ A compact hand evaluation, with supporting methods:
 ```julia
 using PlayingCards
 using PokerHandEvaluator
-using PokerHandEvaluator.HandTypes
 table_cards = (J♡,J♣,A♣,A♢);
 player_cards = (
   (A♠,2♠,table_cards...),
@@ -117,25 +120,23 @@ player_cards = (
 );
 che = CompactHandEval.(player_cards);
 winner_id = argmin(hand_rank.(che)) # = 2
-winning_hand = hand_type(che[winner_id]) # = Quads()
+winning_hand = hand_type(che[winner_id]) # = :quads
 ```
 """
-struct CompactHandEval{HT <: AbstractHandType} <: AbstractHandEvaluation
-    hand_type::HT
+struct CompactHandEval <: AbstractHandEvaluation
     rank::Int
     CompactHandEval(player_cards::Tuple, table_cards::Tuple) =
         CompactHandEval((player_cards..., table_cards...))
     CompactHandEval(all_cards...) = CompactHandEval(all_cards)
     function CompactHandEval(all_cards::Tuple)
         @assert 5 ≤ length(all_cards) ≤ 7
-        rank, hand_type = evaluate_compact(all_cards)
-        return new{typeof(hand_type)}(hand_type, rank)
+        return new(evaluate_compact(all_cards))
     end
 end
 
-to_tuple(th::CompactHandEval) = (th.rank, th.hand_type)
+hand_type(th::CompactHandEval) =
+    hand_type_binary_search(hand_rank(th))
 hand_rank(th::CompactHandEval) = th.rank
-hand_type(th::CompactHandEval) = th.hand_type
 
 """
     combinations_6_choose_5(tup::Tuple)
@@ -200,18 +201,44 @@ all possible 5, 6, and 7-card hand combinations.
 """
 function evaluate end
 
+function hand_type_binary_search(rank::Int)
+    @assert 1 ≤ rank ≤ 7462
+    if rank ≤ 1609
+        if rank ≤ 166
+            rank ≤ 10 && return :straight_flush
+            return :quads
+        else
+            rank ≤ 322 && return :full_house
+            rank ≤ 1599 && return :flush
+            return :straight
+        end
+    else
+        if rank ≤ 3325
+            rank ≤ 2467 && return :trips
+            return :two_pair
+        else
+            rank ≤ 6185 && return :one_pair
+            return :high_card
+        end
+    end
+end
 
 #####
 ##### evaluate_full
 #####
 
 # Wrapper for 5 card hands:
-evaluate_full(t::Tuple{<:Card,<:Card,<:Card,<:Card,<:Card}) = (evaluate5(t)..., t)
+function evaluate_full(t::Tuple{<:Card,<:Card,<:Card,<:Card,<:Card})
+    hand_rank = evaluate5(t)
+    return (hand_rank, hand_type_binary_search(hand_rank), t)
+end
 
 # Wrapper for 6 card hands:
 function evaluate_full(t::Tuple{<:Card,<:Card,<:Card,<:Card,<:Card,<:Card})
     hand_ranks = map(combinations_6_choose_5(t)) do cards
-        (evaluate5(cards)..., cards)
+        hand_rank = evaluate5(cards)
+        hand_type = hand_type_binary_search(hand_rank)
+        (hand_rank, hand_type, cards)
     end
     i_max = argmin(map(x->x[1][1], hand_ranks))
     return hand_ranks[i_max]
@@ -219,7 +246,9 @@ end
 # Wrapper for 7 card hands:
 function evaluate_full(t::Tuple{<:Card,<:Card,<:Card,<:Card,<:Card,<:Card,<:Card})
     hand_ranks = map(combinations_7_choose_5(t)) do cards
-        (evaluate5(cards)..., cards)
+        hand_rank = evaluate5(cards)
+        hand_type = hand_type_binary_search(hand_rank)
+        (hand_rank, hand_type, cards)
     end
     i_max = argmin(map(x->x[1][1], hand_ranks))
     return hand_ranks[i_max]
