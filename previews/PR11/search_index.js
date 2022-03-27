b4848d7a061954d3d5ca3ed42e25aef5d3980953
Documenter.jl <documenter@juliadocs.github.io>
2022-03-27 03:42:18 +0000

    build based on aa5b781

---
+++ previews/PR11/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"api/#API","page":"API","title":"API","text":"","category":"section"},{"location":"#PokerHandEvaluator.jl","page":"Home","title":"PokerHandEvaluator.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"A package for evaluating Texas hold 'em hands.","category":"page"}]
+}
---
+++ previews/PR8/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"api/#API","page":"API","title":"API","text":"","category":"section"},{"location":"#HoldemHandEvaluator.jl","page":"Home","title":"HoldemHandEvaluator.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"A package for evaluating Texas hold 'em hands.","category":"page"}]
+}
